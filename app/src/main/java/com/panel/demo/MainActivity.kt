ba86e712db8e177795602bd0db2a67349c1ee452
Tejas A. Prajaapti <tejas.prajapati@zibtek.com>
2022-12-13 20:39:33 +0530

    - add flutter changes

---
+++ app/src/main/java/com/panel/demo/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.panel.demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.panel.demo.databinding.ActivityMainBinding
+import com.tappp.library.view.OverlayPanelLayout
+
+class MainActivity : AppCompatActivity(), OverlayPanelLayout.DataListener {
+    private var binding: ActivityMainBinding? = null
+    private lateinit var overlayPanelLayout: OverlayPanelLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        overlayPanelLayout = OverlayPanelLayout(this)
+
+        overlayPanelLayout.init(supportFragmentManager, R.id.fragment_container)
+
+
+        //overlayPanelLayout.start()
+
+        //overlayPanelLayout.subscribe(this)
+    }
+
+    override fun onDataReceived(call: String?, result: Any?) {
+
+    }
+
+}
